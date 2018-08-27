69e58501c0dcf28b43f477f6e1851d1a924601a7
Ryan Putman <putman10@me.com>
2018-08-27 11:35:41 -0700

    add comments and friends to user constructor

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -1,12 +1,14 @@
-import { BucketList } from './bucket-list.model';
+import { BucketList } from './bucketlist.model';
 
 export class User {
   constructor(
     public age: number,
     public bio: string,
     public bucketlist: BucketList[],
+    public comments: string[],
     public contact: string,
     public email: string,
+    public friends: string[],
     public name: string,
     public password: string,
     public username: string
