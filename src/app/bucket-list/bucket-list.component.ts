c02255ff35520174c26937b97a860bbc9a7c7075
Ryan Putman <putman10@me.com>
2018-08-27 14:27:36 -0700

    add bucketlist html and functions with dynamic checkbox on completion

--- src/app/bucket-list/bucket-list.component.ts
+++ src/app/bucket-list/bucket-list.component.ts
@@ -7,13 +7,19 @@ import { UserService } from '../user.service';
 @Component({
   selector: 'app-bucket-list',
   templateUrl: './bucket-list.component.html',
-  styleUrls: ['./bucket-list.component.css']
+  styleUrls: ['./bucket-list.component.css'],
+  providers: [UserService]
 })
 export class BucketListComponent implements OnInit {
+  userFromDatabase;
+  userFromDatabaseObject;
 
-  constructor() { }
-
+  constructor(private router: Router, private route: ActivatedRoute, private location: Location, private userService: UserService) { }
   ngOnInit() {
-  }
-
+    this.userFromDatabase = this.userService.getUserById("0");
+    this.userFromDatabase.subscribe(dataLastEmittedFromObserver => {
+      this.userFromDatabaseObject = dataLastEmittedFromObserver;
+console.log(this.userFromDatabaseObject);
+    });
+  };
 }
