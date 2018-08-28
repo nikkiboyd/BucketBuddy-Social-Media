d5f2aafe5bf21f0f9b806f9fd756571bd63c0b3e
Student <example@example.com>
2018-08-28 11:00:39 -0700

    rework category to be a set array instead of pulling and querying database

--- src/app/bucket-list/bucket-list.component.ts
+++ src/app/bucket-list/bucket-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
 import { Router } from '@angular/router';
 import { Location } from '@angular/common';
@@ -12,31 +12,19 @@ import { FirebaseListObservable } from 'angularfire2/database';
   styleUrls: ['./bucket-list.component.css'],
   providers: [UserService]
 })
-export class BucketListComponent implements OnInit {
+export class BucketListComponent implements OnInit{
   userFromDatabase;
   userFromDatabaseObject;
   allUsersFromDatabase;
   allUsersFromDatabaseObject;
   userBucketList;
-  allCategories = [];
+  allCategories = ["Achievement", "Adventure",
+"Body & Health","Career","Charity","Creative","Cultural","Events","Family & Kids","Financial","Food & Drink","Personal Development","Relationship","Sports","Travel"];
 
   constructor(private router: Router, private route: ActivatedRoute, private location: Location, private userService: UserService) { }
   ngOnInit() {
     this.allUsersFromDatabase = this.userService.getUsers();
-    let categoryList = [];
-    this.allUsersFromDatabase.subscribe(dataLastEmittedFromObserver => {
-      dataLastEmittedFromObserver.forEach(function(user){
-        for(let r = 0; r < user.bucketlist.length; r++){
-          if(user.bucketlist[r].category !== undefined){
-            categoryList.push(user.bucketlist[r].category);
-          }
-        }
-      });
-      this.allCategories = categoryList;
-      console.log(this.allCategories)
-    });
     this.userBucketList = this.userService.getUserBucketList("0");
-
     this.userFromDatabase = this.userService.getUserById("0");
     this.userBucketList.subscribe(dataLastEmittedFromObserver => {
       this.userFromDatabaseObject = dataLastEmittedFromObserver;
