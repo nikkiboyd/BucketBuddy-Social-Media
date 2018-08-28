ca31522b7f86becc16c5817c42d73bdc82514645
Student <example@example.com>
2018-08-27 17:15:08 -0700

    add new bucket item form to bucketlist component

--- src/app/bucket-list/bucket-list.component.ts
+++ src/app/bucket-list/bucket-list.component.ts
@@ -14,9 +14,11 @@ import { FirebaseListObservable } from 'angularfire2/database';
 export class BucketListComponent implements OnInit {
   userFromDatabase;
   userFromDatabaseObject;
+  allUsersFromDatabase;
 
   constructor(private router: Router, private route: ActivatedRoute, private location: Location, private userService: UserService) { }
   ngOnInit() {
+    this.allUsersFromDatabase = this.userService.getUsers();
     this.userFromDatabase = this.userService.getUserById("0");
     this.userFromDatabase.subscribe(dataLastEmittedFromObserver => {
       this.userFromDatabaseObject = dataLastEmittedFromObserver;
